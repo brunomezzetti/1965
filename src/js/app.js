cc480f06f46bd0da8ac5f6599d88ca80738a6690
Bruno Mezzetti <brunomezzetti@gmail.com>
2019-09-06 15:28:36 -0300

    codigo inicial

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,10 @@
 var config = {
-    apiKey: "AIzaSyCGnOB9DcH-ZxHfd2RYOqMHmnFq1TBGub0",
-    authDomain: "curso-fb.firebaseapp.com",
-    databaseURL: "https://curso-fb.firebaseio.com",
-    storageBucket: "curso-fb.appspot.com",
+    apiKey: "AIzaSyD4ZDA6AJyJARezwoK2ucBZKAH0utZPpnM",
+    authDomain: "updelivery-1234.firebaseapp.com",
+    databaseURL: "https://updelivery-1234.firebaseio.com",
+    projectId: "updelivery-1234",
+    storageBucket: "",
+    messagingSenderId: "750229679301",
+    appId: "1:750229679301:web:02654f10d1ba93cd"
 };
 firebase.initializeApp(config);
